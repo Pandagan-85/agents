29a9afdf4b9707bd85159a4b043781d1112016e5
Veronica Schembri <veronicaschembri@gmail.com>
2025-09-29 18:31:11 +0200

    Complete Week3 crewai, start week 4 LangGraph

---
+++ 3_crew/my_coder/src/my_coder/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+
+from my_coder.crew import MyCoder
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+assignment = """
+    Write a python program to calculate the first 10,000 terms \
+    of this series, multiplying the total by 4: 1 - 1/3 + 1/5 - 1/7 + ...
+"""
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'assignment': assignment
+    }
+
+    result = MyCoder().crew().kickoff(
+        inputs=inputs)
+    print(f"Result: {result.raw}")
